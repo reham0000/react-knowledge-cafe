43fce0f6eb5773c570ad51a735e3b99fd44afc9e
reham0000 <amsarkerreham@gmail.com>
2024-10-25 21:03:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Blogs from './components/blogs/blogs'
+import Header from './components/header/Header'
+
+function App() {
+  
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
