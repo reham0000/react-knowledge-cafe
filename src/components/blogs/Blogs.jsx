58d74ead948f8759503223f428944e2dd7b9b496
reham0000 <amsarkerreham@gmail.com>
2024-10-26 11:42:08 +0600

    bookmark section added

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Blog from "../Blog/Blog";
+import PropTypes from "prop-types";
 
-const Blogs = () => {
+const Blogs = ({handleAddToBookmark}) => {
 
     const [blogs, setBlogs] = useState([]);
 
@@ -17,10 +18,18 @@ const Blogs = () => {
         <div className="md:w-2/3">
             <h1 className="text-4xl">Blogs: {blogs.length}</h1>
             {
-                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                blogs.map(blog => <Blog 
+                    key={blog.id}
+                    blog={blog}
+                    handleAddToBookmark={handleAddToBookmark}>                    
+                </Blog>)
             }
         </div>
     );
 };
+
+Blogs.protoTypes = {
+    handleAddToBookmark: PropTypes.func
+}
 
 export default Blogs;
